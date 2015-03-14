aa4b872f73c57b2909898712f805fbfa0bc34cbc
MetalHeel <MetalHeel@aol.com>
2015-03-14 13:50:24 -0500

    Access without exception swallowing.

--- AccessPoint.rb
+++ AccessPoint.rb
@@ -14,39 +14,40 @@ socket_thread = Thread.new {
 	server = TCPServer.new('localhost', 3001)
 
 	loop do
-		the_socket = server.accept
-		input = the_socket.gets
-		pieces = input.split(' ')
-		request = pieces[1]
+		Thread.new(server.accept) do |the_socket|
+			input = the_socket.gets
+			pieces = input.split(' ')
+			request = pieces[1]
 
-		STDERR.puts request
-		
-		path = "/home/metalheel/Music/" + request
+			STDERR.puts request
+			
+			path = "/home/metalheel/Music/" + request
 
-		if File.exists?(path)
-			file = File.open(path)
-			chunk = file.read()
+			if File.exists?(path)
+				file = File.open(path)
+				chunk = file.read()
 
-			the_socket.print "HTTP/1.1 200 OK\r\n" +
-	               "Content-Type: audio/mpeg\r\n" +
-	               "Connection: close\r\n"
-	        the_socket.print "\r\n"
+				the_socket.print "HTTP/1.1 200 OK\r\n" +
+		               "Content-Type: audio/mpeg\r\n" +
+		               "Connection: close\r\n"
+		        the_socket.print "\r\n"
 
-	        begin
-	        	the_socket.write(chunk)
-	        rescue Exception => ex
-	        	STDERR.puts ex
-	        end
+		        begin
+		        	the_socket.write(chunk)
+		        rescue Exception => ex
+		        	STDERR.puts ex
+		        end
 
-		else
-			response = "I don't know what to do with this.\r\n"
-			the_socket.print "HTTP/1.1 200 OK\r\n" +
-	               "Content-Type: text/plain\r\n" +
-	               "Content-Length: #{response.bytesize}\r\n" +
-	               "Connection: close\r\n"
-	        the_socket.print "\r\n"
-	        the_socket.print response
-	    end
+			else
+				response = "I don't know what to do with this.\r\n"
+				the_socket.print "HTTP/1.1 200 OK\r\n" +
+		               "Content-Type: text/plain\r\n" +
+		               "Content-Length: #{response.bytesize}\r\n" +
+		               "Connection: close\r\n"
+		        the_socket.print "\r\n"
+		        the_socket.print response
+		    end
+		end
 	end
 }
 
